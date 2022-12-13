66b6d79710653470b9dbcffabe69a86688bd8e89
Sofia Borg <fia.borg@hotmail.com>
2022-12-13 13:13:17 +0100

    frontend added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
+  );
+};
