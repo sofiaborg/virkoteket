c94d67bae899e75dba0332bc873772f1984f0882
Sofia Borg <fia.borg@hotmail.com>
2022-12-13 15:21:59 +0100

    k

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,11 @@ import express, { Application, Request, Response } from "express";
 import { json } from "body-parser";
 import userRouter from "./routes/userRoute";
 import cors from "cors";
-import connectDB from "./services/database";
+require("./services/database.ts");
 
 const app: Application = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
-connectDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("you got /");
--- backend/src/services/database.ts
+++ backend/src/services/database.ts
@@ -1,14 +1,23 @@
 import mongoose from "mongoose";
 require("dotenv").config();
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect("mongodb://localhost/virkoteket" || "");
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
+mongoose
+  .connect("mongodb://localhost/virkoteket", {})
+  .then(() => {
+    console.log("Server running");
+  })
+  .catch((err) => {
     console.log(err);
-  }
-};
+  });
 
-export default connectDB;
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect("mongodb://localhost/virkoteket");
+
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// export default connectDB;
