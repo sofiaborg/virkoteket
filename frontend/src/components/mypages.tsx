2e47beea903c0b3fe43e04df61d054f70a501218
Sofia Borg <fia.borg@hotmail.com>
2022-12-14 10:50:23 +0100

    update

---
+++ frontend/src/components/mypages.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Mypages = () => {
+  return (
+    <>
+      <h1>hejsansvejsan</h1>
+      <form action="#">
+        <input type="text" />
+        <input type="text" />
+      </form>
+    </>
+  );
+};
