6747c472165562b0c09293c139049e7e256ba8c8
Sofia Borg <fia.borg@hotmail.com>
2022-12-18 16:33:14 +0100

    added form

--- backend/src/controllers/auth_controllers/logoutUser.ts
+++ backend/src/controllers/auth_controllers/logoutUser.ts
@@ -2,5 +2,6 @@ import express, { Router, Request, Response } from "express";
 require("dotenv").config();
 
 export const logoutUser = async (req: Request, res: Response) => {
-  res.cookie("token", " ", { maxAge: 0 });
+  localStorage.removeItem("token");
+  console.log("logged out");
 };
--- frontend/src/components/pages/mypages.tsx
+++ frontend/src/components/pages/mypages.tsx
@@ -25,20 +25,74 @@ export const Mypages = () => {
   return (
     <>
       <Wrapper>
+        <h1>Mina sidor</h1>
         <div className="sidebar-wrapper">
-          <h1>Mina sidor</h1>
-
           <div>
             <h2>Mina mönster</h2>
-          </div>
-          <div>
-            <h2>Mitt konto</h2>
-          </div>
-          <div>
-            <button onClick={handleLogout}>Logga ut</button>
+            <div>
+              <div>
+                <h2>Mitt konto</h2>
+              </div>
+              <div>
+                <button onClick={handleLogout}>Logga ut</button>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="mypages-wrapper"></div>
+
+        <div className="mainbar-wrapper">
+          <h3>
+            Om mönster finns, loopa ut dem här. "Skapa mönster"-knapp ligger
+            längst ner på sidan
+          </h3>
+          <button>Skapa mönster</button>
+          <h3>Om inga mönster är skapade ännu ska detta form visas direkt</h3>
+
+          <form action="#">
+            <input type="text" placeholder="Namn på mösnter" />
+            <input type="text" placeholder="Beskrining" />
+            <input type="file" placeholder="ladda upp bild" />
+            <input type="file" placeholder="Ladda upp mönster som pdf" />
+            <label>Kategori:</label>
+
+            <select name="cars" id="cars">
+              <option value="volvo">Virka</option>
+              <option value="saab">Sticka</option>
+            </select>
+
+            <h3>Beroende på vald kategori får man olika filter här:</h3>
+            <label>Svårighetsgrad:</label>
+
+            <select name="cars" id="cars">
+              <option value="volvo">Nybörjare</option>
+              <option value="saab">Normal</option>
+              <option value="saab">Erfaren</option>
+            </select>
+            <label>Stickfasthet:</label>
+
+            <select name="cars" id="cars">
+              <option value="volvo">x</option>
+              <option value="saab">x</option>
+              <option value="saab">x</option>
+            </select>
+            <label>Virknål:</label>
+
+            <select name="cars" id="cars">
+              <option value="volvo">x</option>
+              <option value="saab">x</option>
+              <option value="saab">x</option>
+            </select>
+            <label>Garntyp:</label>
+
+            <select name="cars" id="cars">
+              <option value="volvo">x</option>
+              <option value="saab">x</option>
+              <option value="saab">x</option>
+            </select>
+          </form>
+
+          <button>Ladda upp mönster</button>
+        </div>
       </Wrapper>
     </>
   );
