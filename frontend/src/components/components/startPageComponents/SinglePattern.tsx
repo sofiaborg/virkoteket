5da20ead1458dfa24739e1fb6a20d6140d29d079
Sofia Borg <fia.borg@hotmail.com>
2023-01-05 12:22:17 +0100

    k

--- frontend/src/components/components/startPageComponents/SinglePattern.tsx
+++ frontend/src/components/components/startPageComponents/SinglePattern.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { IPost } from "../../../interfaces/IProps";
 import { IReview } from "../../../interfaces/IProps";
-import { Comment } from "./singlePatternComponents/Comment";
+import { getCurrentUser } from "../../../interfaces/IProps";
 
 export const SinglePattern = () => {
   const [post, setPost] = useState<IPost>({
@@ -20,25 +20,63 @@ export const SinglePattern = () => {
     user: "",
     reviews: [],
   });
+  const [rating, setRating] = useState<Number>(0);
+  const [comment, setComment] = useState<String>("");
+  const [image, setImage] = useState<String>("");
+  const [addReview, setAddReview] = useState<Boolean>(false);
   const { id } = useParams();
 
   //fetch pattern
   useEffect(() => {
-    // const headers: Record<string, string> = {
-    //   Authorization: sessionStorage.getItem(user.token) as string,
-    // };
     async function fetchProduct() {
       const response = await fetch(
         `http://localhost:8000/posts/${id}/getsinglepost`
-        // {
-        //   headers,
-        // }
       );
       const data = await response.json();
       setPost(data);
     }
     fetchProduct();
-  }, []);
+  }, [addReview]);
+
+  //set rating state
+  const handleRatingChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRating(Number(event.target.value));
+  };
+
+  //submit comment + rating
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    const user = getCurrentUser();
+
+    event.preventDefault();
+    await fetch(
+      `http://localhost:8000/posts/${id}/createreview`,
+
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          mode: "no-cors",
+        },
+
+        body: JSON.stringify({
+          rating,
+          comment,
+          image,
+          user: user.email,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+
+    setComment("");
+    setImage("");
+    setRating(0);
+
+    setAddReview(true);
+  };
 
   return (
     <>
@@ -63,7 +101,7 @@ export const SinglePattern = () => {
         <div>{post.description}</div>
         <div>{post.type}</div>
         <div>{post.difficulty}</div>
-        {<div>{post.category}</div>}
+        <div>{post.category}</div>
         <div>{post.hook}</div>
         <div>{post.needle}</div>
         <div>{post.yarn}</div>
@@ -71,7 +109,67 @@ export const SinglePattern = () => {
           Hämta mönster som pdf
         </a>
       </div>
-      <Comment></Comment>
+
+      <div className="comment-wrapper">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="textarea"
+            placeholder="Vad tyckte du om mönstret?"
+            onChange={(e) => setComment(e.target.value)}
+          />
+
+          <input
+            type="file"
+            placeholder="Lägg till bild"
+            onChange={(e) => setImage(e.target.value)}
+          />
+
+          {/* behöver lägga till for="...." i varje rate  https://codepen.io/hesguru/pen/BaybqXv */}
+          <div className="rate">
+            <input
+              type="radio"
+              id="rate5"
+              name="rate"
+              value="5"
+              onChange={handleRatingChange}
+            />
+            <label title="text">5</label>
+            <input
+              type="radio"
+              id="rate4"
+              name="rate"
+              value="4"
+              onChange={handleRatingChange}
+            />
+            <label title="text">4</label>
+            <input
+              type="radio"
+              id="rate3"
+              name="rate"
+              value="3"
+              onChange={handleRatingChange}
+            />
+            <label title="text">3</label>
+            <input
+              type="radio"
+              id="rate2"
+              name="rate"
+              value="2"
+              onChange={handleRatingChange}
+            />
+            <label title="text">2</label>
+            <input
+              type="radio"
+              id="rate1"
+              name="rate"
+              value="1"
+              onChange={handleRatingChange}
+            />
+            <label title="text">1</label>
+          </div>
+          <button>Skicka</button>
+        </form>
+      </div>
     </>
   );
 };
