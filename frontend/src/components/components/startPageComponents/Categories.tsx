449498e77cfabcbba56bc8f11c095eea6e38b792
Sofia Borg <fia.borg@hotmail.com>
2023-01-15 19:42:12 +0100

    added active state to cats

--- frontend/src/components/components/startPageComponents/Categories.tsx
+++ frontend/src/components/components/startPageComponents/Categories.tsx
@@ -1,13 +1,17 @@
 import { categoryList } from "../../../interfaces/IProps";
 import { categoryProps } from "../../../interfaces/IProps";
+import { useState } from "react";
 
 export const Categories = (props: categoryProps) => {
+  const [activeCategory, setActiveCategory] = useState<string>("");
+
   return (
     <>
       <div className="category-wrapper">
         <p
           onClick={(e) => {
             props.onCategoryClick("", "");
+            setActiveCategory("");
           }}
           className="uppercase font-sans font-family: sans-open text-sm pb-3 cursor-pointer hover:font-bold focus:font-bold"
         >
@@ -16,10 +20,13 @@ export const Categories = (props: categoryProps) => {
         <ul>
           {categoryList.map((category, i) => (
             <li
-              className="uppercase font-sans font-family: sans-open text-sm pb-3 cursor-pointer hover:font-bold focus:font-bold"
+              className={`uppercase font-sans font-family: sans-open text-sm pb-3 cursor-pointer hover:font-bold ${
+                activeCategory === category.title ? "font-bold" : ""
+              }`}
               key={i}
               value={category.title}
               onClick={(e) => {
+                setActiveCategory(category.title);
                 props.onCategoryClick(category.title, category.description);
               }}
             >
